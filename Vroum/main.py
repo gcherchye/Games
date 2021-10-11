d89f7f015420a18094bca3c7fb793a15c722cd36
GoodyeAAr <gauthier.cherchye@partenamut.be>
2021-10-11 11:26:08 +0200

    feat(vroum): instanciate the main loop

--- Vroum/main.py
+++ Vroum/main.py
@@ -14,3 +14,17 @@ TRACK_BORDER = pygame.image.load('img/track-border.png')
 FINISH = pygame.image.load('img/finish.png')
 RED_CAR = pygame.image.load('img/red-car.png')
 GREEN_CAR = pygame.image.load('img/green-car.png')
+
+# Pygame settings
+WIN = pygame.display.set_mode((TRACK.get_width(), TRACK.get_height()))
+pygame.display.set_caption('Vroum Vroum !!')
+
+# Main loop
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
