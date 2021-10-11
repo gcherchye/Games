697b2ff72b6b005525d0126ce06a4604f0f9832f
GoodyeAAr <gauthier.cherchye@partenamut.be>
2021-10-11 11:17:16 +0200

    feat(vroum): import images

--- Vroum/main.py
+++ Vroum/main.py
@@ -1,2 +1,16 @@
 """Main script of the game"""
 from __future__ import absolute_import
+
+import math
+import time
+
+import pygame
+
+
+# Load images
+GRASS = pygame.image.load('img/grass.jpg')
+TRACK = pygame.image.load('img/track.png')
+TRACK_BORDER = pygame.image.load('img/track-border.png')
+FINISH = pygame.image.load('img/finish.png')
+RED_CAR = pygame.image.load('img/red-car.png')
+GREEN_CAR = pygame.image.load('img/green-car.png')
